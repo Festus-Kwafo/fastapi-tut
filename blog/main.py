5a757e4c567379c1740d58e7a116dae991886f4b
Festus Kwafo <68119922+Festus-Kwafo@users.noreply.github.com>
2022-07-14 13:06:26 +0000

    hashing passwordr

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,10 @@
-from urllib import request, response
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from sqlalchemy import false
 
-from . import models, schemas
+from . import models, schemas, hashing
 from .database import engine, SessionLocal
-from  sqlalchemy.orm import Session
+from sqlalchemy.orm import Session
+
 
 models.Base.metadata.create_all(engine)
 app = FastAPI()
@@ -13,7 +13,6 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'data': {'name': 'Susan'}}
-
 
 
 def get_db():
@@ -25,21 +24,24 @@ def get_db():
 
 
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create_blog(blog: schemas.Blog, db: Session =Depends(get_db)):
-    new_blog = models.Blog(title = blog.title, body=blog.body)
+def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=blog.title, body=blog.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blog')
-def get_blog(db: Session =Depends(get_db)):
+
+@app.get('/blog', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBLog])
+def get_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{blog_id}', status_code=status.HTTP_200_OK)
-def get_single_blog(blog_id:int, response: Response, db: Session = Depends(get_db)  ):
-    single_blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+
+@app.get('/blog/{blog_id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBLog)
+def get_single_blog(blog_id: int, response: Response, db: Session = Depends(get_db)):
+    single_blog = db.query(models.Blog).filter(
+        models.Blog.id == blog_id).first()
     if not single_blog:
         raise HTTPException(status_code=404, detail="Item not found")
         # response.status_code = status.HTTP_404_NOT_FOUND
@@ -47,8 +49,9 @@ def get_single_blog(blog_id:int, response: Response, db: Session = Depends(get_d
 
     return single_blog
 
+
 @app.delete('/blog/{blog_id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(blog_id:int, db: Session = Depends(get_db)):
+def delete_blog(blog_id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         raise HTTPException(status_code=404, detail="Item not found")
@@ -56,11 +59,25 @@ def delete_blog(blog_id:int, db: Session = Depends(get_db)):
     db.commit()
     return "data"
 
+
 @app.put('/blog/{blog_id}', status_code=status.HTTP_202_ACCEPTED)
-def update_blog(blog_id:int, request: schemas.Blog, db: Session =Depends(get_db)):
+def update_blog(blog_id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         raise HTTPException(status_code=404, detail="Item not found")
     blog.update(request, synchronize_session=False)
     db.commit()
     return "Updated Successfully"
+
+
+
+
+@app.post('/user',  status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
+def user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=hashing.Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
--- blog/schemas.py
+++ blog/schemas.py
@@ -5,3 +5,18 @@ from typing import Optional
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBLog(Blog):
+    class Config():
+        orm_mode = True
+
+
+class User(BaseModel):
+    name : str
+    email: str
+    password: str
+
+class ShowUser(User):
+    
+    class Config():
+        orm_mode = True
